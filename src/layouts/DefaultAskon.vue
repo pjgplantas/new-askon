1117f0c4f1f2fd542d11aab058e640d8c09e39e4
pjgplantas <pjgplantas@gmail.com>
2022-09-05 14:42:00 -0300

    footer

--- src/layouts/DefaultAskon.vue
+++ src/layouts/DefaultAskon.vue
@@ -2,13 +2,16 @@
   <div>
     <NavBarAskon/>
     <router-view/>
+    <FooterAskon/>
   </div>
 </template>
 
 <script>
+import FooterAskon from '@/views/FooterAskon';
 import NavBarAskon from "@/components/NavBarAskon.vue";
 export default {
   components: {
+    FooterAskon,
     NavBarAskon,
   },
 };
---
+++ src/views/FooterAskon.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <div class="footer">
+            <div class="footer-esquerda">
+            <ul>
+                <li class="nomes-footer">Rodrigo Bonifácio Conceição</li>
+                <li class="nomes-footer">Guilherme Bragato Albanaz</li>
+                <li class="nomes-footer">Maria Laura Viana</li>
+            </ul>
+        </div>
+            <div class="img-footer"></div>
+            <div class="footer-direita">
+                <ul class="lista-direita">
+                    <li class="links-footer">GitHub do Rodrigo <mdiGithub  class="icone-link" /></li>
+                    <li class="links-footer">Linkedin do Rodrigo <mdiLinkedin class="icone-link" /> </li>
+                    <li class="links-footer">GitHub do Guilherme <mdiGithub  class="icone-link" /></li>
+                    <li class="links-footer">Linkedin do Guilherme <mdiLinkedin class="icone-link" /> </li>
+                    <li class="links-footer"> GitHub da Maria <mdiGithub  class="icone-link" /></li>
+                    <li class="links-footer">Linkedin da Maria <mdiLinkedin class="icone-link" /> </li>
+                </ul>
+            </div>
+        </div>
+    </section>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
